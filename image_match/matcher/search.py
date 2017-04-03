502f23782ed938c824e9cf8b68506d362132bdcc
NickLeoMartin <nickm@overscore.co.za>
2017-04-03 09:25:08 +0200

    Updated search.py to a function

--- image_match/matcher/search.py
+++ image_match/matcher/search.py
@@ -1,10 +1,33 @@
 # import the necessary packages
-from pyimagesearch.colordescriptor import ColorDescriptor
-from pyimagesearch.searcher import Searcher
+from matcher.color_descriptor import ColorDescriptor
+from matcher.searcher import Searcher
 import argparse
 import cv2
 
 # construct the argument parser and parse the arguments
+def search(index_store_path='',query_img_path='',result_path=''):
+    # initialize the image descriptor
+    cd = ColorDescriptor((8, 12, 3))
+
+    # load the query image and describe it
+    query = cv2.imread(query_img_path)
+    features = cd.describe(query)
+
+    # perform the search
+    searcher = Searcher(index_store_path)
+    results = searcher.search(features)
+
+    # display the query
+    cv2.imshow("Query", query)
+
+    # loop over the results
+    for (score, resultID) in results:
+        # load the result image and display it
+        result = cv2.imread(result_path + "/" + resultID)
+        cv2.imshow("Result", result)
+        cv2.waitKey(0)
+
+"""
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--index", required = True,
     help = "Path to where the computed index will be stored")
@@ -34,3 +57,4 @@ for (score, resultID) in results:
     result = cv2.imread(args["result_path"] + "/" + resultID)
     cv2.imshow("Result", result)
     cv2.waitKey(0)
+"""
