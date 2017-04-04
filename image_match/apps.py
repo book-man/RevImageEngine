8d8b3064543189d02c4992f0cb07733ff4f4547d
NickLeoMartin <nickm@overscore.co.za>
2017-04-04 15:23:15 +0200

    Registered celery in appsconfig

--- image_match/apps.py
+++ image_match/apps.py
@@ -5,3 +5,7 @@ from django.apps import AppConfig
 
 class ImageMatchConfig(AppConfig):
     name = 'image_match'
+    verbose_name = 'Image Match'
+
+    def ready(self):
+        import image_match.signals
